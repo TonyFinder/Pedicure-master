c4859114537bd4ab96953f5c54cee680a043b88d
Anton <antonrozdobudko@gmail.com>
2022-09-04 22:23:05 +1000

    Design options have been added.

--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -14,6 +14,7 @@ export const Price = () => {
         <div className={styles.container}>
             <div className={styles.innerContainer}>
                 {/*<Zoom cascade>*/}
+                <div className={styles.priceBlock}>
                     <Typography variant={'h2'}
                                 className={styles.title}>{pricePedicureState.header.toUpperCase()}</Typography>
                     <div className={styles.valueContainer}>
@@ -27,11 +28,30 @@ export const Price = () => {
                             </div>
                         ))}
                     </div>
+                </div>
+
                 {/*</Zoom>*/}
                 {/*<Zoom cascade>*/}
-                    <Typography className={styles.restInfo}><sup>*</sup>{pricePedicureState.restInfo[0]}</Typography>
-                    <Typography className={styles.restInfo}><sup>**</sup>{pricePedicureState.restInfo[1]}</Typography>
+                    {/*<Typography className={styles.restInfo}><sup>*</sup>{pricePedicureState.restInfo[0]}</Typography>
+                    <Typography className={styles.restInfo}><sup>**</sup>{pricePedicureState.restInfo[1]}</Typography>*/}
                 {/*</Zoom>*/}
+
+                <div className={styles.designBlock}>
+                <Typography variant={'h2'}
+                            className={styles.title}>{pricePedicureState.designTitle.toUpperCase()}</Typography>
+                <div className={styles.valueContainer}>
+                    {pricePedicureState.designPrice.map((procedure, index) => (
+                        <div key={index} className={styles.block}>
+                            <Divider className={styles.divider}>
+                                <Chip label={procedure.price} className={styles.chip}/>
+                            </Divider>
+                            <Typography variant={'subtitle1'}
+                                        className={styles.description}>{procedure.item}</Typography>
+                        </div>
+                    ))}
+                </div>
+                </div>
+
             </div>
         </div>
     )
