2c77fbc7acc5cad26e6c4dc2404fb74f0eb1ec20
Anton <antonrozdobudko@gmail.com>
2022-11-07 19:48:06 +1000

    Refactoring for Online component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import styles from './Header.module.scss'
 import {MenuList} from '../MatetialUI/MenuList/MenuList';
-import Button from '@mui/material/Button';
 
 export const Header = () => {
     // console.log("Header")
@@ -14,11 +13,11 @@ export const Header = () => {
             <Toolbar className={styles.toolbar}>
                 <MenuList/>
 
-                <a href={'https://pedicure-cabinet.online/register'}>
+                {/*<a href={'https://pedicure-cabinet.online/register'}>
                     <Button color={'inherit'} size={'large'} style={{padding: 2}}>
                         КУПИТЬ КУРС
                     </Button>
-                </a>
+                </a>*/}
 
             </Toolbar>
         </AppBar>
--- src/components/Online/Buy/Buy.tsx
+++ src/components/Online/Buy/Buy.tsx
@@ -4,29 +4,20 @@ import styles from './Buy.module.scss'
 import {introductionValues} from '../../../data/store/mainPageState';
 import {Button} from '../../common/Button/Button';
 
-// const Zoom = require("react-reveal/Zoom")
-
 export const Buy = () => {
-    // console.log("IntroOnline")
 
     return (
         <div className={styles.container}>
             <div className={styles.innerContainer}>
-                {/*<Zoom cascade>*/}
                     <Typography variant={'h1'}
                                 className={styles.title}>{introductionValues.buy.header.toUpperCase()}</Typography>
-                {/*</Zoom>*/}
-                {/*<Zoom cascade>*/}
                     <Typography variant={'h2'}
                                 className={styles.price}>{introductionValues.buy.price}</Typography>
-                {/*</Zoom>*/}
 
                 <div className={styles.bulletBlock}>
                     {introductionValues.buy.bullet.map(bullet => (
-                                // <Zoom cascade key={bullet.id}>
                                     <Typography key={bullet.id} variant={'h3'}
                                                 className={styles.bullet}>{bullet.value}</Typography>
-                                // </Zoom>
                         )
                     )
                     }
